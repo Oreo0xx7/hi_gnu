550c8bfb60be08c3d81aa40062e7212ab3876095
Oreo0xx7 <wpdls0027@kakao.com>
2022-05-17 20:56:46 +0900

    first commit

---
+++ app/src/main/java/com/example/hignu/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hignu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move_on;
+    private Button btn_move_off;
+    private EditText input_test;
+    Button btn_test;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_move_off = findViewById(R.id.btn_move_off);
+        btn_move_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
+        btn_test = findViewById(R.id.btn_move_on);
+
+    }
+}
---
+++ app/src/main/java/com/example/hignu/SubActivity.java
@@ -0,0 +1,24 @@
+package com.example.hignu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView tv_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        tv_sub.setText(str);
+    }
+}
