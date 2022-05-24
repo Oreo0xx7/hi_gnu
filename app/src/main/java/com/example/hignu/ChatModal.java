11a5c22d3ee3430200a7288c9d2fdce19fce700d
Oreo0xx7 <wpdls0027@kakao.com>
2022-05-24 23:17:46 +0900

    ChatBot_input

---
+++ app/src/main/java/com/example/hignu/ChatModal.java
@@ -0,0 +1,18 @@
+package com.example.hignu;
+
+public class ChatModal {
+
+    private String cnt;
+
+    public String getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(String cnt) {
+        this.cnt = cnt;
+    }
+
+    public ChatModal(String cnt){
+        this.cnt = cnt;
+    }
+}
